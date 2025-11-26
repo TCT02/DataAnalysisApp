d53332e5de9104baa7872d3e2612f52cbc09b00f
TCT02 <terry78913@gmail.com>
2025-11-26 00:23:18 -0800

    First API Call Test for Data
    Sucessfully called NWS for some data at a certain coordinate. It gave a full json format of information.

--- DataAnalysisApp.cpp
+++ DataAnalysisApp.cpp
@@ -1,32 +1,86 @@
 // DataAnalysisApp.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include <stdlib.h>
+#include <string.h>
 #include <iostream>
 #include <curl\curl.h>
 
 CURLcode curl_global_init(long flags);
 
-size_t write_data(void* buffer, size_t size, size_t nmemb, void* userp);
+struct memory {
+    char *memory;
+    size_t size;
+};
+
+static size_t write_data(char *buffer, size_t size, size_t nmemb, void *userp) {
+
+    size_t realSize = size * nmemb;
+    struct memory *mem = (struct memory *)userp;
+
+    char *ptr = (char *)realloc(mem->memory, mem->size + realSize + 1);
+
+    if (ptr == NULL) {
+        return 0;
+    }
+
+    mem->memory = ptr;
+    memcpy(&mem->memory[mem->size], buffer, realSize);
+    mem->size += realSize;
+    mem->memory[mem->size] = 0;
+
+    return realSize;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //std::cout << "Hello World!\n";
     curl_global_init(CURL_GLOBAL_ALL);
 
     CURL* handle;
     CURLcode res;
+    struct memory chunk;
 
+    chunk.memory = NULL;
+    chunk.size = 0;
+
+    struct curl_slist* headers = NULL;
+    
+
+    
     handle = curl_easy_init();
     //curl_global_init(handle);
     if (handle) {
-        curl_easy_setopt(handle, CURLOPT_URL, "https://example.com/");
+        //Call NWS API for weather data based on San Diego's coord location 
+        curl_easy_setopt(handle, CURLOPT_URL, "https://api.weather.gov/points/32.7157,-117.1611");
+        //curl_easy_setopt(handle, CURLOPT_URL, "https://api.weather.gov/points/");
+
+        headers = curl_slist_append(headers, "User-Agent: TCTWeather (terry78913@gmail.com)");
+        headers = curl_slist_append(headers, "Accept: application/geo+json");
+
+        curl_easy_setopt(handle, CURLOPT_HTTPHEADER, headers);
+
+        curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, write_data);
+
+        curl_easy_setopt(handle, CURLOPT_WRITEDATA, &chunk);
+
         res = curl_easy_perform(handle);
-        //curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, write_data);
 
         if (res != CURLE_OK) {
             fprintf(stderr, "curl_easy_perform() returned %s/n", curl_easy_strerror(res));
         }
+        else {
+            char* dataPoint = NULL;
+            printf("We got %d bytes to our callback in memory\n", (int)chunk.size);
+            printf("Memory data: \n%s", chunk.memory);
 
+            dataPoint = strstr(chunk.memory, "forecast");
+            if (dataPoint) {
+                printf("Found Domain at index %d\n", (dataPoint = chunk.memory));
+            }
+        }
+
+        free(chunk.memory);
         curl_easy_cleanup(handle);
     }
 
